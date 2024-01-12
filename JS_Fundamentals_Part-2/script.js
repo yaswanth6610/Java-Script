e9736b96578d4627a161387f2ba1470a2e0fec2b
kotha-aravind0698 <yaswanth.kotha2001@gmail.com>
2024-01-13 04:24:16 +0530

    arrays 50% completed

--- JS_Fundamentals_Part-2/script.js
+++ JS_Fundamentals_Part-2/script.js
@@ -1,5 +1,5 @@
 'use strict';
-
+/*
 let hasDriversLicense = false;
 const passTest = true;
 
@@ -87,3 +87,69 @@ function fruitProcessor(apples, oranges) {
 
 console.log(fruitProcessor(4,5));
 
+
+// Arrays
+
+const friend1 = "yash";
+const friend2 = "dinesh";
+const friend3 = "srinivas";
+
+const friends = ['yash','dinesh','srinivas','pritham'];
+
+//there is one more wau to create an array
+
+const years = new Array(1,2,3,4,5,6,7);
+
+console.log(years);
+
+console.log(friends[3]);
+friends[2] = 'sanjay';
+// friends = ['sanjay'];
+console.log(friends);
+
+let arr1 = ['srinivas','pritham'];
+console.log(arr1);
+arr1 = ['yash','dinesh'];
+console.log(arr1);
+
+// arrays in js can store multiple data types values too
+
+const n1 = 'yashwanth';
+const arr2 = [n1, 'abc', 123, true, null, friends,3+3-1];
+
+console.log(arr2);
+console.log(arr2[5]);
+*/
+// Basic array operations (in-built methonds)
+
+const friends = ['yash','dinesh','srinivas','pritham'];
+
+// add in the end 
+let b = friends.push('ray');
+console.log(b);
+console.log(friends);
+
+// adding in the begining 
+
+let a = friends.unshift('roy');
+console.log(a);
+console.log(friends);
+
+// remove element from the end
+
+let c = friends.pop();
+console.log(c);
+console.log(friends);
+
+// remove element from the beg
+let d = friends.shift();
+console.log(d);
+console.log(friends);
+
+console.log(friends.indexOf('srinivas'));
+console.log(friends.indexOf('bob'));
+
+
+
+console.log(friends.includes('srinivas'));
+console.log(friends.includes('bob'));
